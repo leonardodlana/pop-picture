a65af9ea363c44c7327ca601c69859ea00a2ae23
Leonardo Lana <leonardoduartelana@gmail.com>
2018-09-05 20:19:55 -0300

    Added https certificate

--- PopPicture/app/src/main/java/leonardolana/poppicture/helpers/impl/ServerHelperImpl.java
+++ PopPicture/app/src/main/java/leonardolana/poppicture/helpers/impl/ServerHelperImpl.java
@@ -2,15 +2,38 @@ package leonardolana.poppicture.helpers.impl;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.Volley;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.CookieHandler;
 import java.net.CookieManager;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+import leonardolana.poppicture.R;
 import leonardolana.poppicture.helpers.api.ServerHelper;
 import leonardolana.poppicture.server.ServerRequest;
+
+import static leonardolana.poppicture.server.ServerConstants.BASE_URL;
 
 /**
  * Created by Leonardo Lana
@@ -35,19 +58,21 @@ public class ServerHelperImpl implements ServerHelper {
     private static ServerHelper INSTANCE;
 
     public static ServerHelper getInstance(Context context) {
-        if(context instanceof Activity)
+        if (context instanceof Activity)
             throw new UnsupportedOperationException("To avoid leaks, use only application context");
 
-        if(INSTANCE == null)
+        if (INSTANCE == null)
             INSTANCE = new ServerHelperImpl(context);
 
         return INSTANCE;
     }
 
+    private Context mContext;
     private RequestQueue mRequestQueue;
 
     private ServerHelperImpl(Context context) {
-        mRequestQueue = Volley.newRequestQueue(context);
+        mContext = context;
+        mRequestQueue = Volley.newRequestQueue(context, new HurlStack(null, getSocketFactory()));
         CookieManager cookieManage = new CookieManager();
         CookieHandler.setDefault(cookieManage);
     }
@@ -55,5 +80,64 @@ public class ServerHelperImpl implements ServerHelper {
     @Override
     public void execute(ServerRequest serverRequest) {
         mRequestQueue.add(serverRequest.getRequest());
+    }
+
+    private SSLSocketFactory getSocketFactory() {
+
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X.509");
+            InputStream caInput = mContext.getResources().openRawResource(R.raw.certificate);
+            Certificate ca;
+            try {
+                ca = cf.generateCertificate(caInput);
+            } finally {
+                caInput.close();
+            }
+
+            String keyStoreType = KeyStore.getDefaultType();
+            KeyStore keyStore = KeyStore.getInstance(keyStoreType);
+            keyStore.load(null, null);
+            keyStore.setCertificateEntry("ca", ca);
+
+            String tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm();
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(tmfAlgorithm);
+            tmf.init(keyStore);
+
+            HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+                @Override
+                public boolean verify(String hostname, SSLSession session) {
+                    return hostname.compareTo(BASE_URL) == 0;
+                }
+            };
+
+
+            HttpsURLConnection.setDefaultHostnameVerifier(hostnameVerifier);
+            SSLContext context = null;
+            context = SSLContext.getInstance("TLS");
+
+            context.init(null, tmf.getTrustManagers(), null);
+            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+
+            SSLSocketFactory sf = context.getSocketFactory();
+
+
+            return sf;
+
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
